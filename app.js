a05871dc687d2387c6754291ed6addec9cb647a0
MrGrRabbit <58159733+MrGrRabbit@users.noreply.github.com>
2022-04-19 23:02:01 +0500

    Add files via upload
    connection to mongo data base

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 
 const express = require("express");
+const { Db } = require("mongodb");
 
 const app = express();
 const port = 3000;
 
+const MongoClient = require("mongodb").MongoClient;
+
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+mongoClient.connect(function(err, client){
+
+    const db = client.db("admin");
+
+    db.command({ping: 1}, function(err, result){
+        if (!err) {
+            console.log("Подключение с сервером успешно установлено");
+            console.log(result);
+
+        }
+        else {
+            console.log(err);
+        }
+        client.close();
+        console.log("Подключение закрыто!");
+        
+    });
+});
+/*
 app.get("/", (request, response) => {
     
     response.send("<h2> Сервер работает </h2>")
 });
 
 app.listen(port, () => console.log(`server start, port: ${port}`));
+*/
