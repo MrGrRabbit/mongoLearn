2c7cdf8690674d480adb229605491378b01e571b
MrGrRabbit <MrGrRabbit@yandex.ru>
2022-04-20 14:06:57 +0500

    creating the first collection

--- app.js
+++ app.js
@@ -9,21 +9,31 @@ const MongoClient = require("mongodb").MongoClient;
 
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
 mongoClient.connect(function(err, client){
-
-    const db = client.db("admin");
-
+    // создание - подключение (обращение) к базе данных usersdb
+    const db = client.db("usersdb");
+    // создание - подключение к коллеции users
+    const collection = db.collection("users");
+    // метод command, проверяет подключение к базе данных (наприер: объектом ping);
     db.command({ping: 1}, function(err, result){
+        
         if (!err) {
             console.log("Подключение с сервером успешно установлено");
             console.log(result);
-
         }
         else {
             console.log(err);
         }
+    });
+    // Получение количества документов в коллекции
+    collection.countDocuments((err, result) => {
+
+        if (err) {
+            return console.log("Ошибка: " + err);
+        }
+
+        console.log(`В коллекции users ${result} документов`);
         client.close();
         console.log("Подключение закрыто!");
-        
     });
 });
 /*
