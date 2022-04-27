4bc8bcbb672c0f5c32ff04b32a00b859c5be41ea
MrGrRabbit <58159733+MrGrRabbit@users.noreply.github.com>
2022-04-27 22:51:51 +0500

    Add files via upload
    Using Express and Mongo (learn)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-//const express = require("express");
+const express = require("express");
 
-//const app = express();
+const app = express();
 const port = 3000;
 
 const MongoClient = require("mongodb").MongoClient;
@@ -31,39 +31,45 @@ mongoClient.connect(function(err, client){
         }
 
         console.log(`В коллекции users ${result} документов`);
-
-
-    });
-
-    // добавление объектов - метод insertOne()
-    let user = {name: "Tom", age: 23};
-    collection.insertOne(user, (err, result) => {
-        if (err) {
-
-            return console.log("Ошибка: " + message.err);
-        }
-
-        console.log(result);
-        console.log(user);  
-    });
-
-    // adding objects - method insertMany()
-    let users = [{name: "Bob", age: 34}, {name: "Angela", age: 24}, {name: "Tom", age: 21}];
-
-    collection.insertMany(users, (err, result) => {
-        if (err) {
-
-            return console.log("Ошибка: " + message.err);
-        }
-
-        console.log(result);
-        console.log(user);  
         client.close();
         console.log("Подключение закрыто!");
     });
-
+});
+/*
+app.get("/", (request, response) => {
+    
+    response.send("<h2> Сервер работает </h2>")
 });
 
+app.listen(port, () => console.log(`server start, port: ${port}`));
+
+const express = require("express");
+const { Db } = require("mongodb");
+
+const app = express();
+const port = 3000;
+
+const MongoClient = require("mongodb").MongoClient;
+
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+mongoClient.connect(function(err, client){
+
+    const db = client.db("admin");
+
+    db.command({ping: 1}, function(err, result){
+        if (!err) {
+            console.log("Подключение с сервером успешно установлено");
+            console.log(result);
+
+        }
+        else {
+            console.log(err);
+        }
+        client.close();
+        console.log("Подключение закрыто!");
+        
+    });
+});
 /*
 app.get("/", (request, response) => {
     
